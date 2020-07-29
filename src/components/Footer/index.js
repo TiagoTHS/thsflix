511ae7ba4e3b4b5cf47c462ec2441b8d7ac9a832
Tiago Souza <tiago.souza9935@gmail.com>
2020-07-29 18:20:05 -0300

    Add infos pessoais

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,11 +7,20 @@ function Footer() {
       <a href="https://www.alura.com.br/">
         <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
       </a>
+      <a class="icone" href="https://github.com/TiagoTHS">
+        <i class="fab fa-github"></i>
+      </a>
       <p>
         Orgulhosamente criado durante a
         {' '}
         <a href="https://www.alura.com.br/">
           Imersão React da Alura
+        </a>
+        {' '}
+        por
+        {' '}
+        <a href="https://twitter.com/_tiagossouza">
+          Tiago Souza
         </a>
       </p>
     </FooterBase>
